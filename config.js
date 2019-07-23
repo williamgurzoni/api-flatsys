fe0fb536f529ddce3015eb0e0e3372ffb74e887f
William Gurzoni <william_luizat@hotmail.com>
2019-07-22 22:56:09 -0300

    ajuste config

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  user: 'masterUsername',
+  host: 'rds-postgresql-flatsys.cvyoaybgw94g.us-east-2.rds.amazonaws.com',
+  database: 'masterUsername',
+  password: '890uw9t8wy3n4d9-tq94tynq34t89k',
+  port: 5432,
+}
